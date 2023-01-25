7bc02294863d04eb7a5f01b3b0ddc65b0956c298
Jose7420 <7962446@alu.murciaeduca.es>
2023-01-25 10:29:37 +0100

    update front

--- src/components/react-admin/admin.js
+++ src/components/react-admin/admin.js
@@ -1,25 +1,44 @@
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-import { UserList } from 'components/react-admin/users';
-import { PostList, PostEdit, PostCreate } from 'components/react-admin/posts';
-import { MigrationList } from 'components/react-admin/migrations';
+import jsonapiClient from 'ra-jsonapi-client';
 
+import { UserList } from 'components/react-admin/users';
+import { CustomerList, CustomerEdit, CustomerCreate } from 'components/react-admin/customers';
+
+// TODO  eliminar las dos lineas siguientes
+import { PostList, PostEdit, PostCreate } from 'components/react-admin/posts';
+import { MigrationList, MigrationEdit, MigrationCreate } from 'components/react-admin/migrations';
+
+import CustomerIcon from '@mui/icons-material/SupportAgent';
 import PostIcon from '@mui/icons-material/Book';
 import UserIcon from '@mui/icons-material/Group';
 import MigrationIcon from '@mui/icons-material/Storage';
 
 import { AdminLayout } from 'components/react-admin/adminLayout';
 
-const dataProvider = jsonServerProvider('http://encuentro.test/api/records'); //https://jsonplaceholder.typicode.com
+import { ArtworkList} from 'components/react-admin/artworks';
+import ArtworkIcon from '@mui/icons-material/Palette';
+
+//const dataProvider = jsonServerProvider('http://encuentro.test/api');
+const dataProvider = jsonapiClient('http://encuentro.test/api');
+
+<Resource name="customers" list={CustomerList} icon={CustomerIcon} edit={CustomerEdit} create={CustomerCreate} />
 
 const RAdmin = () => (
   <Admin
     basename="/dashboard"
     dataProvider={dataProvider}
     layout={AdminLayout}
-  >
+   >
+    <Resource name="customers"
+    s list={CustomerList}
+     icon={CustomerIcon} edit={CustomerEdit} 
+     create={CustomerCreate} />
 
-    <Resource name="migrations" list={MigrationList} icon={MigrationIcon} recordRepresentation="migration" />
+<Resource name="artworks" list={ArtworkList} icon={ArtworkIcon} />
+
+    <Resource name="migrations"
+      list={MigrationList} icon={MigrationIcon} edit={MigrationEdit} create={MigrationCreate}/>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation="name" />
   </Admin>
